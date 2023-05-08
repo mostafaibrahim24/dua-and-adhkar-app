62accc7cca28a0eb1535e54038b7546a12dd3e29
mostafaibrahim24 <mi.mostafaibrahim@gmail.com>
2023-05-08 21:59:47 +0300

    Add Main Activity

---
+++ app/src/main/java/com/example/duaandadhkar/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.duaandadhkar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); //For night mode theme
+        setContentView(R.layout.activity_main);
+    }
+}
